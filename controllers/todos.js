e2c39430b5041fd8a5329c4be806fc4b137bd18b
jarl <jarl-ivor.burget@voco.ee>
2024-12-12 10:43:17 +0200

    Read todos from file and write todos into file

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,25 +1,38 @@
 import { Todo } from '../models/todo.js'
+import { fileManager } from '../utils/files.js'
 
 class todoController {
     constructor(){
         //hold todo objects in array
-        this.TODOS = []  
+        this.initTODOS() 
     } 
 
-    createTodo(req, res){
-        // get data from POST request
+    async createTodo(req, res){
+        //get data from POST request
         const task = req.body.task
         // create new object via Todo model
         // model constructor uses uniq id and task name as parameter
         const newTodo = new Todo(Math.random().toString(), task)
         // add new todo to todos array
         this.TODOS.push(newTodo)
+        // save data to file
+        await fileManager.writeFile('./data/todos.json', this.TODOS)
         // create a correct response
         res.json({
             message: 'created new todo object',
             newTask: newTodo
         })
-    } 
+    }
+
+    async initTodos(){
+        const todosData = await fileManager.readFile('./data/todos.json')
+        // if data is ok - add file content to array
+        if(todosData !== null){
+            this.TODOS = todosData
+        } else  {
+            this.TODOS = [] // if we do not get data from file create an empty array
+        }
+    }
 
     getTodos(req, res){
         res.json({tasks: this.TODOS})
---
+++ utils/files.js
@@ -0,0 +1,31 @@
+import fs from 'node:fs/promises'
+
+class FileManager {
+
+    async writeFile(filename, data){
+        try {
+            // convert data to JSON format
+            data = JSON.stringify(data, null, 2)
+            // save data info file
+            await fs.writeFile(filename, data)
+        } catch(error){
+            console.log('write error => ', error)
+        }
+    }
+
+    async readFile(filename){
+        try {
+            // content from file in text format
+            const fileContent = await fs.readFile(filename, 'utf8')
+            // convert content from text format to data format
+            const fileData = JSON.parse(fileContent)
+            return fileData
+
+        } catch(error){
+            console.error('read error => ', error)
+            return null
+        }
+    }
+}
+
+export const fileManager = new FileManager()
