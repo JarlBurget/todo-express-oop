a6870f984f8db0090019057fa38de930b9509fb6
jarl <jarl-ivor.burget@voco.ee>
2024-12-12 09:28:45 +0200

    Delete the Todo by id

--- controllers/todos.js
+++ controllers/todos.js
@@ -47,6 +47,26 @@ class todoController {
             updatedTask: this.TODOS[todoIndex]
         })
         }
-    }
+
+        deleteTodo(req, res) {
+            const todoId = req.params.id;
+            const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId);
+    
+            if (todoIndex < 0) {
+                return res.status(404).json({
+                    message: 'Could not find todo with the given ID'
+                });
+            }
+    
+            const deletedTodo = this.TODOS.splice(todoIndex, 1); // Eemalda element massiivist
+            res.json({
+                message: 'Todo deleted successfully',
+                deletedTask: deletedTodo
+            });
+        } 
+    } 
+    
+    
+
 
 export const TodoController = new todoController()
